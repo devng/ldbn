0e18134b16b842118fe3d1a303915f96bcc738f5
devng <n.g.georgiev@gmail.clom>
2015-09-12 06:32:48 +0200

    Version 1.2.1 - Docker Integration

--- src/main/resources/php/config.php
+++ src/main/resources/php/config.php
@@ -4,11 +4,11 @@
  */
 
 // configuration for the database PDO URL, in this case it is a local SQLite3
-$db_url = 'sqlite:/home/ldbn/www/db/ldbn.db';
+$db_url = 'sqlite:/var/ldbn/sql/ldbn.db';
 
 // configuration for the ldbn domain
 $ldbnhost  = 'ldbnonline.com'; //domain without http:// at the begining and / at the end
-$ldbnemail = 'adminldbnonline.com';
+$ldbnemail = 'admin@ldbnonline.com';
 
 // If true an email with an activation link is sent when an user registeres.
 // If false no email is sent and the user can login without activation
